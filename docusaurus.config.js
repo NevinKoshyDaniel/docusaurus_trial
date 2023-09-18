e79760562d581629abaf53fc0f98911d548a7fde
Nevin Daniel <nevinkoshydaniel@gmail.com>
2023-09-19 01:20:45 +0530

    Updates Github Base Url due to build issues

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://nevinkoshydaniel.github.io/docusaurus_trial/',
+  url: 'https://nevinkoshydaniel.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
